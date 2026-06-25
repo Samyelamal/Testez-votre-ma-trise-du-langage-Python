0ca837540b4e1c324006b34364fd6357f88ffcf5
Samyelamal <elamalsamy@gmail.com>
2026-06-25 20:46:31 +0200

    Update main.py
    Removed duplicate test code for the Rectangle class.

--- Exercice09/main.py
+++ Exercice09/main.py
@@ -30,13 +30,6 @@ class Rectangle:
         return 2 * (self.width + self.length)
 
 # Test de la classe Rectangle
-rectangle = Rectangle(5, 3)  # 5:width & 3:length
-print("Largeur:", rectangle.width)
-print("Longueur:", rectangle.length)
-print("Aire:", rectangle.calculate_area())
-print("Périmètre:", rectangle.calculate_perimeter())
-
-# Test de la classe Rectangle
 rectangle = Rectangle(5, 3) # 5:width & 3:length
 print("Largeur:", rectangle.width)
 print("Longueur:", rectangle.length)
