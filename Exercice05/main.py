980ddcb6dec03d1d9a11e83796b3de947a5b4209
Samyelamal <elamalsamy@gmail.com>
2026-06-25 20:39:53 +0200

    Add docstrings to sum and subtraction functions
    Added docstrings to sum and subtraction functions.

--- Exercice05/main.py
+++ Exercice05/main.py
@@ -1,5 +1,22 @@
 def sum(a, b):
+    """
+    Ajoute deux nombres ensemble.
+
+    a : premier nombre
+    b : deuxième nombre
+
+    Retourne : la somme de a et b
+    """
     return a + b
- 
+
+
 def subtraction(a, b):
+    """
+    Soustrait deux nombres.
+
+    a : premier nombre
+    b : deuxième nombre
+
+    Retourne : le résultat de a - b
+    """
     return a - b
