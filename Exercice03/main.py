613d643a034a178c52fd4eb6cb0ca01eceb769c4
Samyelamal <elamalsamy@gmail.com>
2026-06-25 20:26:04 +0200

    Update main.py

--- Exercice03/main.py
+++ Exercice03/main.py
@@ -1,1 +1,10 @@
 words = ["python", "programmation", "langage", "ordinateur", "apprentissage"]
+
+voyelles = "aeiouy"
+
+resultat = [
+    (mot, sum(1 for lettre in mot if lettre in voyelles))
+    for mot in words
+]
+
+print(resultat)
