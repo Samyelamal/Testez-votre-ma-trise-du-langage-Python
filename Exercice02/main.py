787d3dcdb0e351c81201700e4c4d3320fc9591b7
Samyelamal <elamalsamy@gmail.com>
2026-06-25 20:24:39 +0200

    Add student note retrieval and average calculation

--- Exercice02/main.py
+++ Exercice02/main.py
@@ -15,3 +15,19 @@ students = {
          'Histoire': 78
      }
 }
+
+nom = input("Entrez le nom de l’étudiant : ")
+
+if nom in students:
+    print(f"Notes de {nom} :")
+
+    notes = students[nom]
+
+    for matiere, note in notes.items():
+        print(f"{matiere} : {note}")
+
+    moyenne = sum(notes.values()) / len(notes)
+    print(f"Moyenne de {nom} : {moyenne}")
+
+else:
+    print(f"L'étudiant {nom} n'existe pas dans la liste.")
